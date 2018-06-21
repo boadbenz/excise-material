67a49008b6125dfdc0571949b418f260729a88df
Phichat K <phichat_dk@hotmail.com>
2018-06-21 13:29:29 +0700

    add lawbreaker
    - module
    - component.ts
    - html
    - scss

---
+++ src/app/pages/arrests/lawbreaker/lawbreaker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lawbreaker',
+  templateUrl: './lawbreaker.component.html',
+  styleUrls: ['./lawbreaker.component.scss']
+})
+export class LawbreakerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/arrests/lawbreaker/lawbreaker.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LawbreakerComponent } from './lawbreaker.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LawbreakerComponent]
+})
+export class LawbreakerModule { }
