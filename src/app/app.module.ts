f2f9ff7b7cc06f5809be31ae84ff374bd3439321
techlog-excise <techlog.excise@gmail.com>
2018-06-23 01:22:39 +0700

    add file again

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import * as $ from 'jquery';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NavigationComponent } from './shared/header-navigation/navigation.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { RightSidebarComponent } from './shared/right-sidebar/rightsidebar.component';
+import { AppComponent } from './app.component';
+import { routes } from './app.routing';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavigationComponent,
+        BreadcrumbComponent,
+        SidebarComponent,
+        RightSidebarComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot(),
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
