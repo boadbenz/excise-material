d86758b134e6fee038c684e49c5e9878957f56a0
techlog-excise <techlog.excise@gmail.com>
2018-07-21 23:59:35 +0700

    * Change label to match doc

--- src/app/pages/arrests/arrests.service.ts
+++ src/app/pages/arrests/arrests.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
 import { appConfig } from '../../app.config';
 import { Arrest } from './arrest';
 import { ArrestStaff } from './arrest-staff';
@@ -11,180 +15,205 @@ import { ArrestIndictment } from './arrest-indictment';
 
 @Injectable()
 export class ArrestsService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
+  // tslint:disable-next-line:member-ordering
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-    // tslint:disable-next-line:member-ordering
-    private httpOptions = {
-        headers: new HttpHeaders(
-            {
-                'Content-Type': 'application/json'
-            })
-    };
+  async getByKeyword(Textsearch: string): Promise<Arrest[]> {
+    const params = Textsearch;
+    const url = `${appConfig.api7788}/ArrestgetByKeyword`;
 
-    async getByKeyword(Textsearch: string): Promise<Arrest[]> {
-        const params = Textsearch;
-        const url = `${appConfig.api7788}/ArrestgetByKeyword`;
-
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData as Arrest[];
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData as Arrest[];
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async getByConAdv(form: any): Promise<Arrest[]> {
-        const params = JSON.stringify(form);
-        const url = `${appConfig.api7788}/ArrestgetByConAdv`;
+  async getByConAdv(form: any): Promise<Arrest[]> {
+    const params = JSON.stringify(form);
+    const url = `${appConfig.api7788}/ArrestgetByConAdv`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData as Arrest[];
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData as Arrest[];
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async getByCon(ArrestCode: string): Promise<Arrest> {
-        const params = { ArrestCode };
-        const url = `${appConfig.api7788}/ArrestgetByCon`;
+  async getByCon(ArrestCode: string): Promise<Arrest> {
+    const params = { ArrestCode };
+    const url = `${appConfig.api7788}/ArrestgetByCon`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData as Arrest;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData as Arrest;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async updDelete(ArrestCode: string): Promise<any> {
-        const params = { ArrestCode };
-        const url = `${appConfig.api7788}/ArrestupdDelete`;
+  async updDelete(ArrestCode: string): Promise<any> {
+    const params = { ArrestCode };
+    const url = `${appConfig.api7788}/ArrestupdDelete`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async staffupdDelete(StaffID: string): Promise<any> {
-        const params = { StaffID };
-        const url = `${appConfig.api7788}/ArrestStaffupdDelete`;
+  async staffupdDelete(StaffID: string): Promise<any> {
+    const params = { StaffID };
+    const url = `${appConfig.api7788}/ArrestStaffupdDelete`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async lawbreakerupdDelete(LawbreakerID: string): Promise<any> {
-        const params = { LawbreakerID };
-        const url = `${appConfig.api7788}/ArrestLawbreakerupdDelete`;
+  async lawbreakerupdDelete(LawbreakerID: string): Promise<any> {
+    const params = { LawbreakerID };
+    const url = `${appConfig.api7788}/ArrestLawbreakerupdDelete`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async productupdDelete(ProductID: string): Promise<any> {
-        const params = { ProductID };
-        const url = `${appConfig.api7788}/ArrestProductupdDelete`;
+  async productupdDelete(ProductID: string): Promise<any> {
+    const params = { ProductID };
+    const url = `${appConfig.api7788}/ArrestProductupdDelete`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async indicmentupdDelete(IndicmentID: string): Promise<any> {
-        const params = { IndicmentID };
-        const url = `${appConfig.api7788}/ArrestIndicmentupdDelete`;
+  async indicmentupdDelete(IndicmentID: string): Promise<any> {
+    const params = { IndicmentID };
+    const url = `${appConfig.api7788}/ArrestIndicmentupdDelete`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async insAll(Arrest: Arrest): Promise<any> {
-        const params = Arrest;
-        const url = `${appConfig.api7788}/ArrestinsAll`;
+  async insAll(Arrest: Arrest): Promise<any> {
+    const params = Arrest;
+    const url = `${appConfig.api7788}/ArrestinsAll`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async staffinsAll(Staff: ArrestStaff): Promise<any> {
-        const params = Arrest;
-        const url = `${appConfig.api7788}/ArrestStaffinsAll`;
+  async staffinsAll(Staff: ArrestStaff): Promise<any> {
+    const params = Arrest;
+    const url = `${appConfig.api7788}/ArrestStaffinsAll`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async lawbreakerinsAll(lawbreaker: ArrestLawbreaker): Promise<any> {
-        const params = lawbreaker;
-        const url = `${appConfig.api7788}/ArrestLawbreakerinsAll`;
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+  async lawbreakerinsAll(lawbreaker: ArrestLawbreaker): Promise<any> {
+    const params = lawbreaker;
+    const url = `${appConfig.api7788}/ArrestLawbreakerinsAll`;
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async productinsAll(product: ArrestProduct): Promise<any> {
-        const params = product;
-        const url = `${appConfig.api7788}/ArrestProductinsAll`;
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+  async productinsAll(product: ArrestProduct): Promise<any> {
+    const params = product;
+    const url = `${appConfig.api7788}/ArrestProductinsAll`;
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async indicmentinsAll(indicment: ArrestIndictment): Promise<any> {
-        const params = indicment;
-        const url = `${appConfig.api7788}/ArrestIndicmentinsAll`;
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+  async indicmentinsAll(indicment: ArrestIndictment): Promise<any> {
+    const params = indicment;
+    const url = `${appConfig.api7788}/ArrestIndicmentinsAll`;
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
+  }
 
-    async updByCon(Arrest: Arrest): Promise<any> {
-        const params = Arrest;
-        const url = `${appConfig.api7788}/ArrestupdByCon`;
+  async updByCon(Arrest: Arrest): Promise<any> {
+    const params = Arrest;
+    const url = `${appConfig.api7788}/ArrestupdByCon`;
 
-        try {
-            const res = await this.http.post<any>(url, params, this.httpOptions).toPromise();
-            return res.ResponseData;
-        } catch (error) {
-            await alert(error);
-        }
+    try {
+      const res = await this.http
+        .post<any>(url, params, this.httpOptions)
+        .toPromise();
+      return res.ResponseData;
+    } catch (error) {
+      await alert(error);
     }
-
+  }
 }
--- src/app/pages/arrests/list/list.component.ts
+++ src/app/pages/arrests/list/list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { NavigationService } from '../../../shared/header-navigation/navigation.service';
 import { ArrestsService } from '../arrests.service';
@@ -7,99 +14,105 @@ import { Message } from 'app/config/message';
 import { toLocalShort } from 'app/config/dateFormat';
 import { pagination } from 'app/config/pagination';
 @Component({
-    selector: 'app-list',
-    templateUrl: './list.component.html'
+  selector: 'app-list',
+  templateUrl: './list.component.html'
 })
 export class ListComponent implements OnInit, OnDestroy {
+  private subOnSearch: any;
+  paginage = pagination;
+  dataTable: any;
+  advSearch: any;
 
-    private subOnSearch: any;
-    paginage = pagination;
-    dataTable: any;
-    advSearch: any;
+  arrestList = new Array<Arrest>();
+  arrest = new Array<Arrest>();
 
-    arrestList = new Array<Arrest>();
-    arrest = new Array<Arrest>();
+  @ViewChild('arrestTable') arrestTable: ElementRef;
 
-    @ViewChild('arrestTable') arrestTable: ElementRef;
+  constructor(
+    private navService: NavigationService,
+    private arrestService: ArrestsService,
+    private router: Router,
+    private chRef: ChangeDetectorRef
+  ) {
+    // set false
+    this.navService.setEditButton(false);
+    this.navService.setDeleteButton(false);
+    this.navService.setPrintButton(false);
+    this.navService.setSaveButton(false);
+    this.navService.setCancelButton(false);
+    this.navService.setNextPageButton(false);
+    // set true
+    this.navService.setSearchBar(true);
+    this.navService.setNewButton(true);
+    this.advSearch = this.navService.showAdvSearch;
+  }
 
-
-    constructor(
-        private navService: NavigationService,
-        private arrestService: ArrestsService,
-        private router: Router,
-        private chRef: ChangeDetectorRef
-    ) {
-        // set false
-        this.navService.setEditButton(false);
-        this.navService.setDeleteButton(false);
-        this.navService.setPrintButton(false);
-        this.navService.setSaveButton(false);
-        this.navService.setCancelButton(false);
-        this.navService.setNextPageButton(false);
-        // set true
-        this.navService.setSearchBar(true);
-        this.navService.setNewButton(true);
-        this.advSearch = this.navService.showAdvSearch;
-
-    }
-
-    ngOnInit() {
-        this.subOnSearch = this.navService.searchByKeyword.subscribe(async Textsearch => {
-            if (Textsearch) {
-                await this.navService.setOnSearch('');
-                this.onSearch(Textsearch);
-            }
-        })
-    }
-
-    onSearch(Textsearch: any) {
-        this.arrestService.getByKeyword(Textsearch).then(res => this.onSearchComplete(res));
-    }
-
-    onAdvSearch(form: any) {
-
-        const sDateCompare = new Date(form.value.OccurrenceDateFrom);
-        const eDateCompare = new Date(form.value.OccurrenceDateTo);
-
-        if (sDateCompare.getTime() > eDateCompare.getTime()) {
-            alert(Message.checkDate);
-        } else {
-            form.value.DateStartFrom = sDateCompare.getTime();
-            form.value.DateStartTo = eDateCompare.getTime();
-            this.arrestService.getByConAdv(form.value).then(res => this.onSearchComplete(res));
+  ngOnInit() {
+    this.subOnSearch = this.navService.searchByKeyword.subscribe(
+      async Textsearch => {
+        if (Textsearch) {
+          await this.navService.setOnSearch('');
+          this.onSearch(Textsearch);
         }
+      }
+    );
+  }
+
+  onSearch(Textsearch: any) {
+    this.arrestService
+      .getByKeyword(Textsearch)
+      .then(res => this.onSearchComplete(res));
+  }
+
+  onAdvSearch(form: any) {
+    const sDateCompare = new Date(form.value.OccurrenceDateFrom);
+    const eDateCompare = new Date(form.value.OccurrenceDateTo);
+
+    if (sDateCompare.getTime() > eDateCompare.getTime()) {
+      alert(Message.checkDate);
+    } else {
+      //   form.value.DateStartFrom = sDateCompare.getTime();
+      //   form.value.DateStartTo = eDateCompare.getTime();
+      this.arrestService
+        .getByConAdv(form.value)
+        .then(res => this.onSearchComplete(res));
+    }
+  }
+
+  onSearchComplete(list: Arrest[]) {
+    if (!list.length) {
+      alert(Message.noRecord);
+      return false;
     }
 
-    onSearchComplete(list: Arrest[]) {
-        if (!list.length) {
-            alert(Message.noRecord);
-            return false;
-        }
+    this.arrest = [];
+    list.map((p, i) => {
+      p.RowsId = i + 1;
+      p.OccurrenceDate = toLocalShort(p.OccurrenceDate);
+      p.ArrestStaff.map(staff => {
+        staff.FullName = `${staff.TitleName} ${staff.FirstName} ${
+          staff.LastName
+        }`;
+      });
+    });
+    this.arrest = list;
 
-        this.arrest = [];
-        list.map((p, i) => {
-            p.RowsId = i + 1;
-            p.OccurrenceDate = toLocalShort(p.OccurrenceDate);
-            p.ArrestStaff.map(staff => {
-                staff.FullName = `${staff.TitleName} ${staff.FirstName} ${staff.LastName}`;
-            });
-        })
-        this.arrest = list;
+    // set total record
+    this.paginage.TotalItems = this.arrest.length;
+  }
 
-        // set total record
-        this.paginage.TotalItems = this.arrest.length;
+  clickView(code: string) {
+    this.router.navigate([`/arrest/manage/R/${code}`]);
+  }
 
-    }
+  async pageChanges(event: any) {
+    this.arrestList = await this.arrest.slice(
+      event.startIndex - 1,
+      event.endIndex
+    );
+  }
 
-    clickView(code: string) {
-        this.router.navigate([`/arrest/manage/R/${code}`]);
-    }
-
-    async pageChanges(event: any) {
-        this.arrestList = await this.arrest.slice(event.startIndex - 1, event.endIndex);
-    }
-
-    ngOnDestroy() {
-        this.subOnSearch.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.subOnSearch.unsubscribe();
+  }
 }
