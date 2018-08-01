26761f07c930eb0da9e6e8ce4fee8ffcfb178955
Phichat K <phichat_dk@hotmail.com>
2018-08-01 22:31:21 +0700

    u

--- src/app/pages/notices/list/list.component.ts
+++ src/app/pages/notices/list/list.component.ts
@@ -3,17 +3,11 @@ import { Router } from '@angular/router';
 import { NavigationService } from '../../../shared/header-navigation/navigation.service';
 import { NoticeService } from '../notice.service';
 import { Message } from '../../../config/message';
-<<<<<<< HEAD
-import { HttpErrorResponse } from '@angular/common/http';
-import { Notice } from '../notice';
-import { pagination } from '../../../config/pagination';
-=======
 import { Notice } from '../notice';
 import { pagination } from '../../../config/pagination';
 import { toLocalShort } from '../../../config/dateFormat';
 import { PreloaderService } from '../../../shared/preloader/preloader.component';
 import { SidebarService } from '../../../shared/sidebar/sidebar.component';
->>>>>>> FL_J
 
 @Component({
     selector: 'app-list',
@@ -51,10 +45,6 @@ export class ListComponent implements OnInit {
         this.advSearch = this.navservice.showAdvSearch;
     }
 
-<<<<<<< HEAD
-    ngOnInit() {
-        this.subOnSearch = this.navservice.searchByKeyword.subscribe(async Textsearch => {
-=======
     async ngOnInit() {
 
         this.sidebarService.setVersion('1.02');
@@ -65,7 +55,6 @@ export class ListComponent implements OnInit {
         this.preLoaderService.setShowPreloader(false);
 
         this.navservice.searchByKeyword.subscribe(async Textsearch => {
->>>>>>> FL_J
             if (Textsearch) {
                 await this.navservice.setOnSearch(null);
                 this.onSearch(Textsearch);
@@ -75,19 +64,10 @@ export class ListComponent implements OnInit {
         this.preLoaderService.setShowPreloader(false);
     }
 
-<<<<<<< HEAD
-    onSearch(Textsearch: any) {
-        this.noticeService.getByKeyword(Textsearch).subscribe(list => {
-            this.onSearchComplete(list)
-        }, (err: HttpErrorResponse) => {
-            alert(err.message);
-        });
-=======
     async onSearch(Textsearch: any) {
         this.preLoaderService.setShowPreloader(true);
         await this.noticeService.getByKeyword(Textsearch).then(list => this.onSearchComplete(list));
         this.preLoaderService.setShowPreloader(false);
->>>>>>> FL_J
     }
 
     async onAdvSearch(form: any) {
@@ -103,17 +83,6 @@ export class ListComponent implements OnInit {
         if (sDateCompare.valueOf() > eDateCompare.valueOf()) {
             alert(Message.checkDate);
         } else {
-<<<<<<< HEAD
-            form.value.DateStartFrom = sDateCompare.getTime();
-            form.value.DateStartTo = eDateCompare.getTime();
-            this.noticeService.getByConAdv(form.value).subscribe(list => {
-
-                this.onSearchComplete(list)
-
-            }, (err: HttpErrorResponse) => {
-                alert(err.message);
-            });
-=======
             this.preLoaderService.setShowPreloader(true);
 
             form.value.DateStartFrom = sDateCompare.toISOString();
@@ -121,25 +90,12 @@ export class ListComponent implements OnInit {
             await this.noticeService.getByConAdv(form.value).then(list => this.onSearchComplete(list));
 
             this.preLoaderService.setShowPreloader(false);
->>>>>>> FL_J
         }
     }
 
-    onSearchComplete(list: any) {
+    async onSearchComplete(list: any) {
         this.notice = [];
 
-<<<<<<< HEAD
-        if (!list) {
-            alert(Message.noRecord);
-            return false;
-        }
-
-        if (Array.isArray(list)) {
-            this.notice = list;
-        } else {
-            this.notice.push(list);
-        }
-=======
         if (!list.length) {
             alert(Message.noRecord)
             return false;
@@ -156,7 +112,6 @@ export class ListComponent implements OnInit {
                 s.SuspectFullName = `${s.SuspectTitleName} ${s.SuspectFirstName} ${s.SuspectLastName}`;
             })
         })
->>>>>>> FL_J
 
         // set total record
         // this.invesPaginate.TotalItems = this.notice.length;
