73ba286660075e40dd56253faf73e6a0879515b0
Sarawut <Sarawut@192.168.1.6>
2018-10-15 23:56:40 +0700

    fix response data

--- src/app/pages/lawsuit/manage/manage.component.ts
+++ src/app/pages/lawsuit/manage/manage.component.ts
@@ -54,30 +54,29 @@ export class ManageComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.4');
+    this.sidebarService.setVersion('0.0.0.5');
     // this.preLoaderService.setShowPreloader(true);
     await this.getParamFromActiveRoute();
     this.navigate_service();
 
     // this.preLoaderService.setShowPreloader(false);
   }
+
   private navigate_service() {
     this.onPrintSubscribe = this.navService.onPrint.subscribe(async status => {
         if (status) {
             await this.navService.setOnPrint(false);
             this.modal = this.ngbModel.open(this.printDocModel, { size: 'lg', centered: true });
         }
-    })
+    });
     this.onSaveSubscribe = this.navService.onSave.subscribe(async status => {
       if(status) {
         await this.navService.setOnSave(false);
         this.onSave();
       }
     });
+  }
 
-
-    
-  } 
   private setShowButton() {
     this.navService.setPrintButton(false);
     this.navService.setNewButton(false);
@@ -92,7 +91,7 @@ export class ManageComponent implements OnInit {
     this.getDataFromListPage = this.activeRoute.queryParams.subscribe(
       async params => {
         this.preLoaderService.setShowPreloader(true);
-        
+
 
         // LawsuitArrestgetByCon
         await this.lawsuitService.LawsuitArrestGetByCon(params.IndictmentID).then(res => {
@@ -113,15 +112,19 @@ export class ManageComponent implements OnInit {
               staff.FullName = `${staff.TitleName}${staff.FirstName} ${staff.LastName}`;
             });
           });
-          
-          
+
+          console.log(this.lawsuitList);
+
           if(res) {
             this.lawsuitList.map((data, index) => {
               data.RowsId = index + 1;
             });
             let IsLawsuitComplete = res[0]['IsLawsuitComplete'];
-            console.log("ISLAWSUIT:"+this.lawsuitList[0].IsLawsuit);
-            if(IsLawsuitComplete == 1) {
+            console.log("ISLAWSUIT:" + this.lawsuitList[0].IsLawsuit);
+            if(IsLawsuitComplete == 1
+                && (this.lawsuitList || []).length > 0
+                && (this.lawsuitList[0]['LawsuitArrestIndicment'] || []).length > 0
+                && (this.lawsuitList[0]['LawsuitArrestIndicment'][0]['Lawsuit'] || []).length > 0 ) {
               console.log("LAWSUIT COMPLETE = 1");
               this.lawsuitService.MasDocumentMaingetAll(4,params.LawsuitID).then(docall => {
                 console.log(JSON.stringify(docall));
@@ -144,7 +147,7 @@ export class ManageComponent implements OnInit {
               this.lawsuitList[0].AccuserTestimony = AccuserTestimony;
               let isProve = this.lawsuitList[0]['LawsuitArrestIndicment'][0]['IsProve'];
               let lawsuitType = this.lawsuitList[0]['LawsuitArrestIndicment'][0]['LawsuitArrestIndicmentDetail'][0]['LawsuitType'];
-              
+
               if(isProve == 0) {
                 if(lawsuitType == 1) {
                   this.navService.setPrintButton(true);
@@ -175,18 +178,19 @@ export class ManageComponent implements OnInit {
               this.navService.setCancelButton(true);
             }
           }
-      
+
+
           /*if (res) {
             this.lawsuitList.map((data, index) => {
               data.RowsId = index + 1;
             });
-           
+
             if (res && res.IsOutside == 1 && res.LawsuitNo != null) {
               this.lawsuitList.map(law => {
                 law.LawsuitNo = `น ${law.LawsuitNo}`;
               });
             }
-           
+
             if (res && res.IsLawsuit == 0) {
               this.lawsuitList.map(law => {
                 law.IsLawsuitStatus = "ไม่รับคดี";
@@ -200,7 +204,7 @@ export class ManageComponent implements OnInit {
                 law.IsLawsuitStatus = "ยังไม่ดำเนินกำรรับคดี";
               });
             }
-          }*/ 
+          }*/
         });
         /*
         // Find guiltbaseID with IndictmentID from Lawsuit
@@ -244,7 +248,7 @@ export class ManageComponent implements OnInit {
     this.onSaveSubscribe.unsubscribe();
   }
   private async createLawsuitForm() {
-    
+
     this.lawsuitForm = new FormGroup({
       //'LawsuitNo': new FormControl(null, Validators.required),
       //'LawsuitDate': new FormControl(null, Validators.required),
