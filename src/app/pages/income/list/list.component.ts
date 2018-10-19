0d8a517e4ad7edeaee08f2c68ce6410af98f0b99
techlog-excise <40315207+techlog-excise@users.noreply.github.com>
2018-10-19 23:42:36 +0700

    Revenue V 0.0.0.8

--- src/app/pages/income/list/list.component.ts
+++ src/app/pages/income/list/list.component.ts
@@ -57,7 +57,7 @@ export class ListComponent implements OnInit, OnDestroy {
     }
 
     async ngOnInit() {
-        this.sidebarService.setVersion('Revenue 0.0.0.7');
+        this.sidebarService.setVersion('Revenue 0.0.0.8');
 
         this.RevenueStatus = "";
 
