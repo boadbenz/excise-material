f4f43d5888c489c1fbc5c1e1834a4fe56c05a9c7
Phichat K <phichat_dk@hotmail.com>
2018-10-20 18:23:20 +0700

    add fine routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -56,6 +56,13 @@ export const routes: Routes = [
             { path: 'detail/:mode', loadChildren: './pages/lawsuit/detail/detail.module#DetailModule' }
         ]
     }, {
+        path: 'fine', component: LayoutComponent,
+        children: [
+            { path: 'list', loadChildren: './pages/fine/list/list.module#ListModule' },
+            { path: 'manage/:mode/:code1/:code2/:code3', loadChildren: './pages/fine/manage/manage.module#ManageModule' },
+            { path: 'detail', loadChildren: './pages/fine/detail/detail.module#DetailModule' },
+        ]
+    }, {
         path: 'reward', component: LayoutComponent,
         children: [
             { path: 'list', loadChildren: './pages/reward/list/list.module#ListModule' },
