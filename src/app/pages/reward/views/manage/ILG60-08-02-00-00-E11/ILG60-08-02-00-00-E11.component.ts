a5ac9383a5ebaf8e22771355eed3fe31740ad055
nateeteem <nateeteem@gmail.com>
2018-11-09 00:01:30 +0700

    Create New Components And Coding Section2

---
+++ src/app/pages/reward/views/manage/ILG60-08-02-00-00-E11/ILG60-08-02-00-00-E11.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CONFIG } from './CONFIG';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'ILG60-08-02-00-00-E11',
+  templateUrl: './ILG60-08-02-00-00-E11.component.html',
+  styleUrls: ['./ILG60-08-02-00-00-E11.component.scss']
+})
+export class ILG6008020000E11Component extends CONFIG implements OnInit {
+  constructor() {
+    super();
+  }
+
+  ngOnInit() {}
+}
