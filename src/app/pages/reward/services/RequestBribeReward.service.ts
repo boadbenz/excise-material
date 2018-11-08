49080e306b3f1527e9b2bfb7e5154d712053357f
nateeteem <nateeteem@gmail.com>
2018-11-09 00:02:46 +0700

    Add Class RequestBribeRewardgetByIndictmentID

--- src/app/pages/reward/services/RequestBribeReward.service.ts
+++ src/app/pages/reward/services/RequestBribeReward.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HelperService } from './HelperService';
 import { HttpClient } from '@angular/common/http';
+import { IRequestBribeRewardgetByIndictmentID } from '../interfaces/RequestBribeReward.interface';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RequestBribeRewardService extends HelperService {
   constructor(private http: HttpClient) {
     super();
+  }
+
+  public RequestBribeRewardgetByIndictmentID(
+    param: IRequestBribeRewardgetByIndictmentID
+  ): Observable<any> {
+    return this.http.post(
+      `${this.ApiPrefixUrl}/RequestBribeRewardgetByIndictmentID`,
+      param
+    );
   }
 }
