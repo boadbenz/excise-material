afcaea4b3e006e0bce66dae45926b81d313f3851
techlog-excise <techlog.excise@gmail.com>
2018-11-16 10:56:09 +0700

    Update src/app/app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -78,7 +78,7 @@ export const routes: Routes = [
         loadChildren: './pages/prove/list/list.module#ListModule'
       },
       {
-        path: 'manage/:mode/:code1/:code2/:code3/:code4/:code5',
+        path: 'manage/:mode/:code1/:code2',
         loadChildren: './pages/prove/manage/manage.module#ManageModule'
       }
     ]
