5ca3c742fc3a8c36144d7332dc861c17569a8586
nateeteem <nateeteem@gmail.com>
2018-11-17 13:55:26 +0700

    Update Bribe

--- src/app/pages/reward/views/bribe/ILG60-08-03-00-00-E08/ILG60-08-03-00-00-E08.component.ts
+++ src/app/pages/reward/views/bribe/ILG60-08-03-00-00-E08/ILG60-08-03-00-00-E08.component.ts
@@ -7,7 +7,19 @@ import {
   IRequestArrestLawsuit
 } from 'app/pages/reward/interfaces/RequestArrestLawsuit.interface';
 import { BribeService } from '../bribe.service';
+import { DropdownInterface } from 'app/pages/reward/shared/interfaces/dropdown-interface';
+import { IRequestCommand } from 'app/pages/reward/interfaces/RequestCommand';
+import { IRequestBribe } from 'app/pages/reward/interfaces/RequestBribe.interface';
+import { IRequestBribeDetail } from 'app/pages/reward/interfaces/RequestBribeDetail.interface';
 
+interface IRequestBribeDetailCustom extends IRequestBribeDetail {
+  LawbreakerName: string;
+  FineTypeName: string;
+  PaymentDate: string;
+  Receipt: string;
+  BribeMoney: number;
+  NetBribeMoney: number;
+}
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'ILG60-08-03-00-00-E08',
@@ -15,12 +27,50 @@ import { BribeService } from '../bribe.service';
   styleUrls: ['./ILG60-08-03-00-00-E08.component.scss']
 })
 export class ILG6008030000E08Component extends CONFIG implements OnInit {
+  public RequestCommand_NoticeCode_list: DropdownInterface[] = [];
+  public tableDetail: IRequestBribeDetailCustom[] = [];
   constructor(
     private fb: FormBuilder,
     private requestArrestLawsuitService: RequestArrestLawsuitService,
     private bribeService: BribeService
   ) {
     super();
+    this.RequestCommand$.subscribe((resCom: IRequestCommand[]) => {
+      if (resCom) {
+        const RequestCommand: IRequestCommand =
+          resCom.length > 0 ? resCom[0] : {};
+
+        this.RequestCommand_NoticeCode_list = RequestCommand.RequestCommandDetail.map(
+          m => ({
+            text: `${m.NoticeCode || ''}/${m.TitleName || ''} ${m.FirstName ||
+              ''} ${m.LastName || ''}`,
+            value: m.NoticeCode
+          })
+        );
+
+        this.formGroup.controls['RequestBribeCode'].setValue('Auto Generate');
+      }
+    });
+    this.RequestBribe$.subscribe((resBri: IRequestBribe[]) => {
+      if (resBri) {
+        const RequestBribe: IRequestBribe = resBri.length > 0 ? resBri[0] : {};
+        this.tableDetail = RequestBribe.RequestBribeDetail.map(m => ({
+          ...m,
+          LawbreakerName: `${m.LawbreakerTitleName ||
+            ''}${m.LawbreakerFirstName || ''} ${m.LawbreakerMiddleName || ''}
+         ${m.LawbreakerLastName || ''}${m.LawbreakerOtherName || ''} `,
+          FineTypeName: m.FineType === 0 ? 'เปรียบเทียบคดี' : 'ส่งฟ้องศาล',
+          PaymentDate:
+            m.FineType === 0 ? m.PaymentActualDate : m.PaymentDueDate,
+          Receipt: m.FineType === 0 ? m.ReceiptNo : m.JudgementNo,
+          BribeMoney: Number(m.PaymentFine * 0.2),
+          NetBribeMoney:
+            Number(
+              Number(m.PaymentFine * 0.2) / Number(RequestBribe.TotalPart)
+            ) * Number(RequestBribe.PartMoney)
+        }));
+      }
+    });
   }
 
   ngOnInit() {
@@ -41,5 +91,23 @@ export class ILG6008030000E08Component extends CONFIG implements OnInit {
       .subscribe((res: IRequestArrestLawsuit[]) => {
         this.RequestArrestLawsuit$.next(res);
       });
+  }
+  public total() {
+    return {
+      PaymentFine:
+        this.tableDetail.length > 0
+          ? this.tableDetail.map(m => m.PaymentFine).reduce((a, b) => (a += b))
+          : 0,
+      BribeMoney:
+        this.tableDetail.length > 0
+          ? this.tableDetail.map(m => m.BribeMoney).reduce((a, b) => (a += b))
+          : 0,
+      NetBribeMoney:
+        this.tableDetail.length > 0
+          ? this.tableDetail
+              .map(m => m.NetBribeMoney)
+              .reduce((a, b) => (a += b))
+          : 0
+    };
   }
 }
