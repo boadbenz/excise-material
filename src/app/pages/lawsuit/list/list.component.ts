47875814c7f7b59d1b44e0279587227278b858aa
niamsiri <niamsiri.em@gmail.com>
2018-11-18 13:22:29 +0700

    update

--- src/app/pages/lawsuit/list/list.component.ts
+++ src/app/pages/lawsuit/list/list.component.ts
@@ -43,7 +43,7 @@ export class ListComponent implements OnInit, OnDestroy {
     this.advSearch = this.navService.showAdvSearch;
   }
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.7');
+    this.sidebarService.setVersion('0.0.0.8');
     this.paginage.TotalItems = 0;
     this.preLoaderService.setShowPreloader(true);
     // await this.lawsuitService.LawsuitArrestGetByKeyword('').then(list => this.onSearchComplete(list));
@@ -185,11 +185,13 @@ export class ListComponent implements OnInit, OnDestroy {
 
   private viewData(item) {
     // console.log('item==>', item)
+
     if (item.LawsuitArrestIndicment[0].Lawsuit[0]) {
       item.LawsuitID = item.LawsuitArrestIndicment[0].Lawsuit[0].LawsuitID;
     } else {
       item.LawsuitID = '';
     }
+
     this.router.navigate(['/lawsuit/manage', 'R'], {
       queryParams: { IndictmentID: item.LawsuitArrestIndicment[0].IndictmentID, LawsuitID: item.LawsuitID }
     });
