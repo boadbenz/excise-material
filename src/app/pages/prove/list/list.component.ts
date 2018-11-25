aaa617132b3242e162a47277cdc3048ff036c092
techlog-excise <40315207+techlog-excise@users.noreply.github.com>
2018-11-25 13:15:27 +0700

    Prove V 0.0.0.11

--- src/app/pages/prove/list/list.component.ts
+++ src/app/pages/prove/list/list.component.ts
@@ -66,7 +66,7 @@ export class ListComponent implements OnInit {
         this.DeliveryDateTo = null;
         this.ProveDateTo = null;
 
-        this.sidebarService.setVersion('Prove 0.0.0.10');
+        this.sidebarService.setVersion('Prove 0.0.0.11');
 
         //this.preLoaderService.setShowPreloader(true);
         //this.onSearch({ Textsearch: "" });
