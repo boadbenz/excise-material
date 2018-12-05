e1a0d0676c9308bfb83062931adf253252c13d46
Tae <techlog.excise@gmail.com>
2018-12-06 01:37:44 +0700

    Auto stash before revert of "update defect investigate"

---
+++ src/app/pages/reward/models/RequestListgetByConAdv.Model.ts
@@ -0,0 +1,10 @@
+export const RequestListgetByConAdvModel = {
+  ArrestCode: '',
+  LawsuitNo: '',
+  OccurrenceDateFrom: '',
+  OccurrenceDateTo: '',
+  LawsuitDateFrom: '',
+  LawsuitDateTo: '',
+  StaffName: '',
+  OfficeName: ''
+};
