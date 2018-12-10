4eda166dfd4748d280b5e62bb957bb4712e0bef2
techlog-excise <techlog.excise@gmail.com>
2018-12-10 13:43:13 +0700

    no message

--- src/app/pages/notices/manage/manage.component.ts
+++ src/app/pages/notices/manage/manage.component.ts
@@ -145,7 +145,7 @@ export class ManageComponent implements OnInit, OnDestroy {
         });
         this.preloader.setShowPreloader(true);
 
-        this.sidebarService.setVersion('0.0.2.28');
+        this.sidebarService.setVersion('0.0.2.29');
 
         this.navigate_service();
 
