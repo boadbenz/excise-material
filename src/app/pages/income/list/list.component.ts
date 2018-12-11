7de889a1ebbcafb148d8815d7e7388e1d31cdcf8
techlog-excise <40315207+techlog-excise@users.noreply.github.com>
2018-12-11 10:57:51 +0700

    Prove & Revenue V 0.0.0.22

--- src/app/pages/income/list/list.component.ts
+++ src/app/pages/income/list/list.component.ts
@@ -58,7 +58,7 @@ export class ListComponent implements OnInit, OnDestroy {
     }
 
     async ngOnInit() {
-        this.sidebarService.setVersion('Revenue 0.0.0.21');
+        this.sidebarService.setVersion('Revenue 0.0.0.22');
         this.RevenueStatus = "";
         //this.advSearch.next(true);
         //this.preloader.setShowPreloader(true);
