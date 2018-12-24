087c9d1553c78530e89e5f4a065e292843671309
Phichat K <phichat_dk@hotmail.com>
2018-12-24 14:55:44 +0700

    Update arrest.service.ts

--- src/app/pages/arrests/services/arrest.service.ts
+++ src/app/pages/arrests/services/arrest.service.ts
@@ -16,7 +16,7 @@ export class ArrestService {
     ) {
     }
 
-    version = '0.0.0.47'
+    version = '0.0.0.48'
 
     private httpOptions = {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
