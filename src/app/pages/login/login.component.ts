e760ddd359eb8bc210ce35004321c9d3e820bf67
toey_dev <techlog.excise@gmail.com>
2019-01-08 18:45:20 +0700

    v 0.0.2.50

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
   }
 
   async onSubmit(form: any) {
-    var User = form.userName
+    var User = form.userName;
     var Pass = form.password;
     if (!User) {
       this.errMsg = "กรุณาระบุชื่อเข้าระบบ";
@@ -47,72 +47,73 @@ export class LoginComponent implements OnInit {
 
         //***********************************Used with in the Excise Only (Don't Delete)********************************** */
 
-        const params = {
-          systemId: "WSS",
-          UserName: User,
-          Password: Pass,
-          ipAddress: "10.11.1.10",
-          requestData: {
-            UserId: "lic02"
-          }
-        };
-        await this.authService.ssoService(params).subscribe(async res => {
-          if (res.ResponseMessage == "SUCCESS") {
-            // console.log("res++++ : ", res);
-            await this.authService.userAndPrivilegeInfo(User).subscribe(async res => {
-              // console.log("getpin : ", res);
-              if (res.pin) {
-                // console.log("Hi pin : ", res.pin)
-                const paramsPinID = {
-                  SystemID: "sso",
-                  Username: "pccsso",
-                  Password: "sso2561",
-                  RequestData: {
-                    PersonNID: res.pin
-                  }
-                }
-                if (User == Pass) {
-                  await this.authService.eofficeInfo(paramsPinID).subscribe(async res => {
-                    // console.log("res eofficeInfo  : ", res);
-                    if (res.Status == "OK") {
-                      this.fullName = res.PersonInformation.PersonTHTitle + " " +
-                        res.PersonInformation.PersonTHName + " " + res.PersonInformation.PersonTHSurName;
-                      localStorage.setItem('fullName', this.fullName);
-                      localStorage.setItem('operationPosName', res.PersonInformation.LinePotistion);
-                      localStorage.setItem('officeShortName', res.PersonInformation.WorkOffName);
-                      let url = decodeURIComponent(this.returnUrl);
-                      this.router.navigateByUrl(url);
-                    } else this.errMsg = "Please Check User or Password "
-                  });
-                } else this.errMsg = "User or Password invalid"
-              } else this.errMsg = "User or Password invalid"
-            });
+        // const params = {
+        //   systemId: "WSS",
+        //   UserName: User,
+        //   Password: Pass,
+        //   ipAddress: "10.11.1.10",
+        //   requestData: {
+        //     UserId: "lic02"
+        //   }
+        // };
+        // await this.authService.ssoService(params).subscribe(async res => {
+        //   if (res.ResponseMessage == "SUCCESS") {
+        //     console.log("res ssoService : ", res);
+        //     await this.authService.userAndPrivilegeInfo(User).subscribe(async res => {
+        //       console.log("getpin : ", res);
+        //       if (res.pin) {
+        //         // console.log("Hi pin : ", res.pin)
+        //         const paramsPinID = {
+        //           SystemID: "sso",
+        //           Username: "pccsso",
+        //           Password: "sso2561",
+        //           RequestData: {
+        //             PersonNID: res.pin
+        //           }
+        //         }
+        //         if (User == Pass) {
+        //           await this.authService.eofficeInfo(paramsPinID).subscribe(async res => {
+        //              console.log("res eofficeInfo  : ", res);
+        //             if (res.Status == "OK") {
+        //               this.fullName = res.PersonInformation.PersonTHTitle + " " +
+        //                 res.PersonInformation.PersonTHName + " " + res.PersonInformation.PersonTHSurName;
+        //               localStorage.setItem('fullName', this.fullName);
+        //               localStorage.setItem('operationPosName', res.PersonInformation.LinePotistion);
+        //               localStorage.setItem('officeShortName', res.PersonInformation.WorkOffName);
+        //               let url = decodeURIComponent(this.returnUrl);
+        //               this.router.navigateByUrl(url);
+        //             } else this.errMsg = "Please Check User or Password "
+        //           });
+        //         } else this.errMsg = "User or Password invalid"
+        //       } else this.errMsg = "User or Password invalid"
+        //     });
 
-          } else this.errMsg = res.ResponseMessage;// console.log("res.ErrMsg ++++ : ", res.ResponseMessage);
-        });
+        //   } else this.errMsg = res.ResponseMessage;// console.log("res.ErrMsg ++++ : ", res.ResponseMessage);
+        // });
 
         //****************************(End Used with in the Excise Only)***************************** */
 
 
         /*****************************Used outside the Excise (Don't Delete*)****************************** */
-        // const params = {
-        //   UserName: User,
-        //   Password: Pass
-        // };
-        // await this.authService.userAuth(params).subscribe(async res => {
-        //   if (res.StaffCode != null) {
-        //     this.fullName = res.TitleName + " " + res.FirstName + " " + res.LastName;
-        //     this.operationPosName = res.OperationPosName;
-        //     this.OfficeShortName = res.OfficeShortName;
-        //     localStorage.setItem('fullName', this.fullName);
-        //     localStorage.setItem('operationPosName', this.operationPosName);
-        //     localStorage.setItem('officeShortName', this.OfficeShortName);
-        //     localStorage.setItem('staffCode', res.StaffCode);
-        //     localStorage.setItem('officeCode', res.OfficeCode);
-        //     let url = decodeURIComponent(this.returnUrl);
-        //     this.router.navigateByUrl(url);
-        //   } else this.errMsg = res.Msg;
-        // });
+        const params = {
+          UserName: User,
+          Password: Pass
+        };
+        await this.authService.userAuth(params).subscribe(async res => {
+          console.log("res++++ : ", res);
+          if (res.StaffCode != null) {
+            this.fullName = res.TitleName + " " + res.FirstName + " " + res.LastName;
+            this.operationPosName = res.OperationPosName;
+            this.OfficeShortName = res.OfficeShortName;
+            localStorage.setItem('fullName', this.fullName);
+            localStorage.setItem('operationPosName', this.operationPosName);
+            localStorage.setItem('officeShortName', this.OfficeShortName);
+            localStorage.setItem('staffCode', res.StaffCode);
+            localStorage.setItem('officeCode', res.OfficeCode);
+            let url = decodeURIComponent(this.returnUrl);
+            this.router.navigateByUrl(url);
+          } else this.errMsg = res.Msg;
+        });
         /*****************************End Used outside the Excise****************************** */
       }
     }
