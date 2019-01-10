5fdc6a914142c971ae07a468e361a2ce287836a0
techlog-excise <40315207+techlog-excise@users.noreply.github.com>
2019-01-10 23:09:42 +0700

    Login

--- src/app/pages/login/auth.service.ts
+++ src/app/pages/login/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+import { appConfig } from "app/app.config";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpService } from "app/core/http.service";
+import { Observable } from 'rxjs';
+import { Http, Response, RequestOptions, Headers, Jsonp, ResponseContentType } from '@angular/http'
+import { stringify } from '@angular/compiler/src/util';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient,
+    private HttpService: HttpService,
+    private http: Http) { }
+
 
   signin(form: any) {
-    
+
     // return res.map(user => {s
     //   if (user && user.access_token) {
-        localStorage.setItem('currentUser', JSON.stringify(form));
+    localStorage.setItem('currentUser', JSON.stringify(form));
     //   }
-      return form;
+    return form;
     // });
   }
 
@@ -20,4 +29,48 @@ export class AuthService {
     localStorage.removeItem('currentUser');
   }
 
+  userAuth(params): Observable<any> {
+    let options = new RequestOptions({ headers: this.getHeaders() });
+    const url = `${appConfig.api8086}/UACVerifyLogIn`
+    return this.http.post(url, params, options)
+      .map((res: Response) => res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  /****************************(Used with in the Excise Only)***************************** */
+  ssoService(params): Observable<any> {
+    let options = new RequestOptions({ headers: this.getHeaders() });
+    const url = `${appConfig.exciseService}/EDRestServicesUAT/sso/ExciseUserInfomation`
+    return this.http.post(url, params, options)
+      .map((res: Response) => res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  userAndPrivilegeInfo(User) {
+    const url = `${appConfig.exciseService}/edssows/ldap/userAndPrivilegeInformation?userID=${User}&systemID=Test010"`
+    return this.http.get(url)
+      .map((res: Response) => res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  eofficeInfo(params): Observable<any> {
+    let options = new RequestOptions({ headers: this.getHeaders() });
+    const url = `http://uat.eoffice.excise.go.th:7003/EOfficeWS/HrstPersonInformation `
+    return this.HttpService.post(url, params, options)
+      .map((res: Response) => res.json())
+      .catch(this.handleErrorObservable);
+  }
+  /****************************(End Used with in the Excise Only)***************************** */
+
+
+  private getHeaders() {
+    let headers = new Headers();  
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    return headers;
+  }
+
+  private handleErrorObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, ReplaySubject } from 'rxjs/Rx';
+import { ThaiDatePipe } from '../reward/pipes/thaiDate.pipe';
+import { from } from 'rxjs/observable/from';
+import { LayoutComponent } from '../../shared/layout/layout.component';
+import { async } from 'q';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [LayoutComponent]
 })
 export class LoginComponent implements OnInit {
 
-  errMsg: string;
-
   returnUrl: string;
 
+  errMsg: string;
+  fullName: string = "";
+  operationPosName: string = "";
+  OfficeShortName: string = "";
   constructor(private authService: AuthService,
     private router: Router,
+    private http: HttpClient,
     private route: ActivatedRoute) {
     // reset signin status
     this.authService.signout();
@@ -22,33 +33,97 @@ export class LoginComponent implements OnInit {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  ngOnInit() {
-  }
-
-  onSubmit(form: any) {
-    var User = form.userName
+  async onSubmit(form: any) {
+    var User = form.userName;
     var Pass = form.password;
-    // console.log("User : ", User, "Pass : ", Pass);
     if (!User) {
       this.errMsg = "กรุณาระบุชื่อเข้าระบบ";
     } else if (!Pass) {
       this.errMsg = "กรุณาระบุรหัสผ่าน";
     }
     if (User && Pass) {
-      if (this.authService.signin(form)) {
-        this.router.navigate([this.returnUrl]);
-      };
+      if (this.authService.signin(from)) {
+
+
+        //***********************************Used with in the Excise Only (Don't Delete)********************************** */
+
+        // const params = {
+        //   systemId: "WSS",
+        //   UserName: User,
+        //   Password: Pass,
+        //   ipAddress: "10.11.1.10",
+        //   requestData: {
+        //     UserId: "lic02"
+        //   }
+        // };
+        // await this.authService.ssoService(params).subscribe(async res => {
+        //   if (res.ResponseMessage == "SUCCESS") {
+        //     // console.log("res++++ : ", res);
+        //     await this.authService.userAndPrivilegeInfo(User).subscribe(async res => {
+        //       // console.log("getpin : ", res);
+        //       if (res.pin) {
+        //         // console.log("Hi pin : ", res.pin)
+        //         const paramsPinID = {
+        //           SystemID: "sso",
+        //           Username: "pccsso",
+        //           Password: "sso2561",
+        //           RequestData: {
+        //             PersonNID: res.pin
+        //           }
+        //         }
+        //         if (User == Pass) {
+        //           await this.authService.eofficeInfo(paramsPinID).subscribe(async res => {
+        //             // console.log("res eofficeInfo  : ", res);
+        //             if (res.Status == "OK") {
+        //               this.fullName = res.PersonInformation.PersonTHTitle + " " +
+        //                 res.PersonInformation.PersonTHName + " " + res.PersonInformation.PersonTHSurName;
+        //               localStorage.setItem('fullName', this.fullName);
+        //               localStorage.setItem('operationPosName', res.PersonInformation.LinePotistion);
+        //               localStorage.setItem('officeShortName', res.PersonInformation.WorkOffName);
+        //               let url = decodeURIComponent(this.returnUrl);
+        //               this.router.navigateByUrl(url);
+        //             } else this.errMsg = "Please Check User or Password "
+        //           });
+        //         } else this.errMsg = "User or Password invalid"
+        //       } else this.errMsg = "User or Password invalid"
+        //     });
+
+        //   } else this.errMsg = res.ResponseMessage;// console.log("res.ErrMsg ++++ : ", res.ResponseMessage);
+        // });
+
+        //****************************(End Used with in the Excise Only)***************************** */
+
+
+        /*****************************Used outside the Excise (Don't Delete*)****************************** */
+        const params = {
+          UserName: User,
+          Password: Pass
+        };
+        await this.authService.userAuth(params).subscribe(async res => {
+          if (res.StaffCode != null) {
+            this.fullName = res.TitleName + " " + res.FirstName + " " + res.LastName;
+            this.operationPosName = res.OperationPosName;
+            this.OfficeShortName = res.OfficeShortName;
+            localStorage.setItem('fullName', this.fullName);
+            localStorage.setItem('operationPosName', this.operationPosName);
+            localStorage.setItem('officeShortName', this.OfficeShortName);
+            localStorage.setItem('staffCode', res.StaffCode);
+            localStorage.setItem('officeCode', res.OfficeCode);
+            let url = decodeURIComponent(this.returnUrl);
+            this.router.navigateByUrl(url);
+          } else this.errMsg = res.Msg;
+        });
+        /*****************************End Used outside the Excise****************************** */
+      }
     }
   }
+
   ClearErrMsg() {
     this.errMsg = '';
   }
 
-  // onSubmit(form: any) {
-  //   if (this.authService.signin(form)) {
-  //     this.router.navigate([this.returnUrl]);
-  //   };
+  ngOnInit() { }
 
-  // }
+
 
 }
