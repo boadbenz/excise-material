6525232764ee96160eabbcf612b460e97564e942
toey_dev <techlog.excise@gmail.com>
2019-01-18 15:47:16 +0700

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,20 +18,24 @@ export const appConfig = {
     // apiReport:'http://192.168.3.134:80/Report_XCS',
     // mockapi: 'http://demo1735762.mockable.io/XCS60',
     // exciseService: 'http://webtest.excise.go.th'
+
+
+    
+
 //-----------------------------------------------------------------//
     apiUrl:  'http://103.233.193.94:8777/XCS60',   
-    api7788: 'http://103.233.193.94:7788/XCS60',  
-    // api8082: 'http://103.233.193.94:8082/XCS60', 
+    api7788: 'http://103.233.193.94:7788/XCS60',      
     api8882: 'http://103.233.193.94:8882/XCS60',    
     api8883: 'http://103.233.193.94:8883/XCS60',
     api8086: 'http://103.233.193.94:8086/XCS60',  
     api8887: 'http://103.233.193.94:8887/XCS60', 
     api8777: 'http://103.233.193.94:8777/XCS60',
     api8776: 'http://103.233.193.94:8776/XCS60',   
-    apiReport:'http://103.233.193.94/Report_XCS',
+    apiReport:'http://103.233.193.94:8000/Report_XCS',
     mockapi: 'http://demo1735762.mockable.io/XCS60',
     exciseService: 'http://webtest.excise.go.th'
     //*******/This is api stoped on colo server********//
+    // api8082: 'http://103.233.193.94:8082/XCS60',
     // api8888: 'http://103.233.193.94:8888/XCS60',
     // api8083: 'http://103.233.193.94:8083/XCS60',
     // api7789: 'http://103.233.193.94:7789/XCS60',
@@ -41,10 +45,11 @@ export const appConfig = {
 
 
 
+
+//-----------------------------------------------------------------//
     //for training
     // apiUrl:  'http://172.30.0.72:8888/XCS60',   
     // api7788: 'http://172.30.0.72:7788/XCS60',  
-    // api8082: 'http://172.30.0.72:8082/XCS60', 
     // api8882: 'http://172.30.0.72:8882/XCS60',    
     // api8883: 'http://172.30.0.72:8883/XCS60',
     // api8086: 'http://172.30.0.72:8086/XCS60',  
