464357fa380f30e004ddb9d3da1d33b244efff3f
niamsiri <niamsiri.em@gmail.com>
2019-02-06 13:13:46 +0700

    ver 34 fix last fixed

--- src/app/pages/lawsuit/manage/manage.component.ts
+++ src/app/pages/lawsuit/manage/manage.component.ts
@@ -122,7 +122,7 @@ export class ManageComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.33');
+    this.sidebarService.setVersion('0.0.0.34');
     this.preLoaderService.setShowPreloader(true);
     await this.getParamFromActiveRoute();
     await this.navigate_service();
