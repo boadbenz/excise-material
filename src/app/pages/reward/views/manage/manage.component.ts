c532f23b3233bb515457793519e4657c671ce0c5
Na TeeM <nateem@3DSs-MacBook-Pro-4.local>
2019-02-12 23:10:30 +0700

    Clean Code

--- src/app/pages/reward/views/manage/manage.component.ts
+++ src/app/pages/reward/views/manage/manage.component.ts
@@ -123,7 +123,7 @@ export class ManageComponent extends ManageConfig implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.sidebarService.setVersion('0.0.1.9');
+    this.sidebarService.setVersion('0.0.1.10');
     this.pageLoad();
   }
 
