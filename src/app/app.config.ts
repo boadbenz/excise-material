bd7be31de06eb6aaa44155e95eb97260aae49324
toey_dev <techlog.excise@gmail.com>
2019-02-14 10:24:55 +0700

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 export const appConfig = {
-     
-    // api7788: 'http://192.168.3.158:7788/XCS60',      
-    // api8882: 'http://192.168.3.158:8882/XCS60',    
-    // api8883: 'http://192.168.3.158:8883/XCS60',
-    // api8086: 'http://192.168.3.158:8086/XCS60',  
-    // api8887: 'http://192.168.3.158:8887/XCS60', 
+
+    // api7777: 'http://192.168.3.158:7777/XCS60',
+    // api7788: 'http://192.168.3.158:7788/XCS60',
+    // api8086: 'http://192.168.3.158:8086/XCS60',
     // api8777: 'http://192.168.3.158:8777/XCS60',
     // api8776: 'http://192.168.3.158:8776/XCS60', 
     // api8775: 'http://192.168.3.158:8775/XCS60',  
-    // apiReport:'http://192.168.3.158/Report_XCS',
+    // apiReport:'http://192.168.3.158:8000/Report_XCS',
     // exciseService: 'http://webtest.excise.go.th'
 
 
