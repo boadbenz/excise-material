ae11ffdd006014a81a53a7eb1f0bfc2c656495c2
toey_dev <techlog.excise@gmail.com>
2019-02-20 15:59:41 +0700

    Enable input reward/reward

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,25 @@
 export const appConfig = {
 
-    // api7777: 'http://192.168.3.158:7777/XCS60',
-    // api7788: 'http://192.168.3.158:7788/XCS60',
-    // api8086: 'http://192.168.3.158:8086/XCS60',
-    // api8777: 'http://192.168.3.158:8777/XCS60',
-    // api8776: 'http://192.168.3.158:8776/XCS60', 
-    // api8775: 'http://192.168.3.158:8775/XCS60',  
-    // apiReport:'http://192.168.3.134/Report_XCS',
-    // exciseService: 'http://webtest.excise.go.th'
+    api7777: 'http://192.168.3.158:7777/XCS60',
+    api7788: 'http://192.168.3.158:7788/XCS60',
+    api8086: 'http://192.168.3.158:8086/XCS60',
+    api8777: 'http://192.168.3.158:8777/XCS60',
+    api8776: 'http://192.168.3.158:8776/XCS60', 
+    api8775: 'http://192.168.3.158:8775/XCS60',  
+    apiReport:'http://192.168.3.134/Report_XCS',
+    exciseService: 'http://webtest.excise.go.th'
 
 
 //-----------------------------------------------------------------//
     
   
-    api7777: 'http://103.233.193.94:7777/XCS60',
-    api7788: 'http://103.233.193.94:7788/XCS60',
-    api8086: 'http://103.233.193.94:8086/XCS60',
-    api8777: 'http://103.233.193.94:8777/XCS60',
-    api8776: 'http://103.233.193.94:8776/XCS60', 
-    api8775: 'http://103.233.193.94:8775/XCS60',  
-    apiReport:'http://103.233.193.62:8000/Report_XCS',
-    exciseService: 'http://webtest.excise.go.th'
+    // api7777: 'http://103.233.193.94:7777/XCS60',
+    // api7788: 'http://103.233.193.94:7788/XCS60',
+    // api8086: 'http://103.233.193.94:8086/XCS60',
+    // api8777: 'http://103.233.193.94:8777/XCS60',
+    // api8776: 'http://103.233.193.94:8776/XCS60', 
+    // api8775: 'http://103.233.193.94:8775/XCS60',  
+    // apiReport:'http://103.233.193.62:8000/Report_XCS',
+    // exciseService: 'http://webtest.excise.go.th'
     
 }
