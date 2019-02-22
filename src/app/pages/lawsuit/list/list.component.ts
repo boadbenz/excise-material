12e4e528b7129408f9a634497e46a289e3febfdb
niamsiri <niamsiri.em@gmail.com>
2019-02-22 10:27:05 +0700

    ver 44

--- src/app/pages/lawsuit/list/list.component.ts
+++ src/app/pages/lawsuit/list/list.component.ts
@@ -50,7 +50,7 @@ export class ListComponent implements OnInit, OnDestroy {
     this.advSearch = this.navService.showAdvSearch;
   }
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.43');
+    this.sidebarService.setVersion('0.0.0.44');
     await this.onSearchByKeyword()
     await this.setShowButton();
     // await this.onNextPage()
