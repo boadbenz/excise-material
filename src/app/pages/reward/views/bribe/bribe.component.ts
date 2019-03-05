44e00682b7b81e8ebb6c7009f379e328f6412e22
nateem <nateeteem@gmail.com>
2019-03-06 00:48:54 +0700

    Defect No.7

--- src/app/pages/reward/views/bribe/bribe.component.ts
+++ src/app/pages/reward/views/bribe/bribe.component.ts
@@ -366,7 +366,7 @@ export class BribeComponent extends BribeConfig implements OnInit, OnDestroy {
       });
   }
   ngOnInit() {
-    this.sidebarService.setVersion('0.0.1.14');
+    this.sidebarService.setVersion('0.0.1.15');
 
     // ILG60-08-03-00-00-E01 (Page Load)
     this.pageLoad();
