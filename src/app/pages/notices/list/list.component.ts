b5e0f3d18f3a814ca28cc18193899d5066f62763
techlog-excise <techlog.excise@gmail.com>
2019-03-12 19:07:59 +0700

    no message

--- src/app/pages/notices/list/list.component.ts
+++ src/app/pages/notices/list/list.component.ts
@@ -66,7 +66,7 @@ export class ListComponent implements OnInit, OnDestroy {
     }
 
     async ngOnInit() {
-        this.sidebarService.setVersion('0.0.2.45');
+        this.sidebarService.setVersion('0.0.2.46');
         localStorage.setItem('programcode', 'ILG60-02-00');
         this.paginage.TotalItems = 0;
 
