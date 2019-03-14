bfe142f882c25c112683d58c185b69fb93359d47
baskub <phittayajenkhetkarn@gmail.com>
2019-03-15 00:00:57 +0700

    v 0.0.0.52 number input

--- src/app/pages/fine/list/list.component.ts
+++ src/app/pages/fine/list/list.component.ts
@@ -121,7 +121,7 @@ export class ListComponent implements OnInit, OnDestroy {
         }, 100);
       }
     async ngOnInit() {
-        this.sidebarService.setVersion('0.0.0.51');
+        this.sidebarService.setVersion('0.0.0.52');
         const form = new FormGroup({
             ArrestCode: new FormControl(''),
             LawsuitCode: new FormControl(''),
