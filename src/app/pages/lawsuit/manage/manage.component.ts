a7999c0c73b5a48cb4112ab73533de03dafec347
niamsiri <niamsiri.em@gmail.com>
2019-03-21 10:16:19 +0700

    0.0.0.53

--- src/app/pages/lawsuit/manage/manage.component.ts
+++ src/app/pages/lawsuit/manage/manage.component.ts
@@ -123,7 +123,7 @@ export class ManageComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.52');
+    this.sidebarService.setVersion('0.0.0.53');
     this.preLoaderService.setShowPreloader(true);
     await this.getParamFromActiveRoute();
     await this.navigate_service();
