db72eb705ca4476104d76eabde7c852937a5d3f5
techlog-excise <techlog.excise@gmail.com>
2019-03-22 21:02:05 +0700

    Update manage.component.ts

--- src/app/pages/evidenceOut/manage/manage.component.ts
+++ src/app/pages/evidenceOut/manage/manage.component.ts
@@ -1997,7 +1997,7 @@ export class ManageComponent implements OnInit, OnDestroy {
                 this.rawProductOptions.map(f => {
                     f.EvidenceOutInItem.forEach(element => {
                         let LawsuitNo = "";
-                        if(f.EvidenceOutProve[0].EvidenceOutLawsuit){
+                        if(f.EvidenceOutProve.length > 0 && f.EvidenceOutProve[0].EvidenceOutLawsuit){
                             LawsuitNo = f.EvidenceOutProve[0].EvidenceOutLawsuit.LawsuitNo;
                         }
                         
