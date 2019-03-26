fc2361c0f7c3a2eba2737437615a75cd97c12fa7
baskub <techlog.excise@gmail.com>
2019-03-26 10:53:07 +0700

    v 0.0.0.54 fix link if no params

--- src/app/pages/fine/list/list.component.ts
+++ src/app/pages/fine/list/list.component.ts
@@ -121,7 +121,7 @@ export class ListComponent implements OnInit, OnDestroy {
         }, 100);
       }
     async ngOnInit() {
-        this.sidebarService.setVersion('0.0.0.53');
+        this.sidebarService.setVersion('0.0.0.54');
         const form = new FormGroup({
             ArrestCode: new FormControl(''),
             LawsuitCode: new FormControl(''),
@@ -285,8 +285,10 @@ export class ListComponent implements OnInit, OnDestroy {
         this.paginage.TotalItems = this.CompareList.length;
     }
 
-    clickView(IndictmentID: string = '', ArrestCode: string = '', CompareID: string = '') {
+    clickView(IndictmentID: string = '0', ArrestCode: string = '0', CompareID: string = '0') {
         // CompareID = (+CompareID)
+        // alert('here');
+        console.log(`/fine/manage/${((!CompareID || CompareID == '0') ? 'C' : 'R')}/${CompareID}/${IndictmentID}/${ArrestCode}`);
         try {
             this._router.navigate([`/fine/manage/${((!CompareID || CompareID == '0') ? 'C' : 'R')}/${CompareID}/${IndictmentID}/${ArrestCode}`]);
         } catch (err) {
