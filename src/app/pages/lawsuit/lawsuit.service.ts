19b54a257256903fff693b915b1ab0997829a225
Worauwt <niamsiri.em@gmail.com>
2019-03-30 13:46:16 +0700

    update init search

--- src/app/pages/lawsuit/lawsuit.service.ts
+++ src/app/pages/lawsuit/lawsuit.service.ts
@@ -54,7 +54,7 @@ export class LawsuitService {
     return this.responsePromiseGetWithoutStatus(JSON.stringify(params), url)
   }
   async LawsuitArrestGetByKeyword(Textsearch: any, AccountOfficeCode: any): Promise<Lawsuit[]> {
-    const params = Textsearch === '' ? { 'Textsearch': '', 'AccountOfficeCode': '' } : { Textsearch: Textsearch, AccountOfficeCode: AccountOfficeCode };
+    const params = Textsearch === '' ? { 'Textsearch': '', 'AccountOfficeCode': AccountOfficeCode } : { Textsearch: Textsearch, AccountOfficeCode: AccountOfficeCode };
     const url = `${appConfig.apiUrl}/LawsuitArrestgetByKeyword`;
     return this.responsePromiseGetWithoutStatus(JSON.stringify(params), url)
   }
--- src/app/pages/lawsuit/manage/manage.component.ts
+++ src/app/pages/lawsuit/manage/manage.component.ts
@@ -123,7 +123,7 @@ export class ManageComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.sidebarService.setVersion('0.0.0.53');
+    this.sidebarService.setVersion('0.0.0.54');
     this.preLoaderService.setShowPreloader(true);
     await this.getParamFromActiveRoute();
     await this.navigate_service();
