73053afecffc47836f8d82e6ac0f5b294c730315
baskub <phittayajenkhetkarn@gmail.com>
2019-03-31 06:12:02 +0700

    v 0.0.0.53 all compare defect

--- src/app/pages/fine/list/list.component.ts
+++ src/app/pages/fine/list/list.component.ts
@@ -121,7 +121,7 @@ export class ListComponent implements OnInit, OnDestroy {
         }, 100);
       }
     async ngOnInit() {
-        this.sidebarService.setVersion('0.0.0.52');
+        this.sidebarService.setVersion('0.0.0.53');
         const form = new FormGroup({
             ArrestCode: new FormControl(''),
             LawsuitCode: new FormControl(''),
